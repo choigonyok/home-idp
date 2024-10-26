20c25fbf569bf0057b7a1e4ebea038d1d06c436a
achoistic98 <achoistic98@naver.com>
2024-10-27 00:52:22 +0900

    chore: update db schema

--- common/postgres.sql
+++ common/postgres.sql
@@ -8,43 +8,54 @@
 CREATE DATABASE home-idp;
 
 CREATE TABLE projects (
-  id SERIAL PRIMARY KEY,
+  id VARCHAR(100) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  creator VARCHAR(100) NOT NULL,
+  creator_id VARCHAR(100) NOT NULL,
   create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE roles (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  id VARCHAR(100) PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE policies (
-  id SERIAL PRIMARY KEY,
+  id VARCHAR(100) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  policy JSON NOT NULL
+  policy JSON NOT NULL,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id VARCHAR(100) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  role_id INTEGER DEFAULT 0,
+  role_id VARCHAR(100) NOT NULL,
   create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE userprojectmapping (
-  user_id INTEGER NOT NULL,
-  project_id INTEGER NOT NULL,
+  user_id VARCHAR(100) NOT NULL,
+  project_id VARCHAR(100) NOT NULL,
   PRIMARY KEY (user_id, project_id),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (project_id) REFERENCES projects(id)
 );
 
 CREATE TABLE rolepolicymapping (
-  role_id INTEGER NOT NULL,
-  policy_id INTEGER NOT NULL,
+  role_id VARCHAR(100) NOT NULL,
+  policy_id VARCHAR(100) NOT NULL,
   PRIMARY KEY (role_id, policy_id),
   FOREIGN KEY (role_id) REFERENCES roles(id),
   FOREIGN KEY (policy_id) REFERENCES policies(id)
+);
+
+CREATE TABLE dockerfiles (
+  id VARCHAR(100) PRIMARY KEY,
+  image_name VARCHAR(100) NOT NULL,
+  image_version VARCHAR(100) NOT NULL,
+  creator VARCHAR(100) NOT NULL,
+  repository VARCHAR(100) NOT NULL,
+  FOREIGN KEY (creator) REFERENCES users(id)
 );
